809f2f6c51cbd2e3a6129b6210f5b599f1424cc0
colibage <78595260+colibage@users.noreply.github.com>
2021-11-30 21:24:32 +0100

    brr

---
+++ Betyg-A-i-matte/Equation.java
@@ -0,0 +1,12 @@
+public class Equation {
+
+    private String equation;
+    private String[] parts;
+
+    public void setEquation(){
+
+
+    }
+
+    //public void splitEquation måste fixa den här
+}
